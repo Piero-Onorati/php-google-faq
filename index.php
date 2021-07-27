100c1d2e08c0294cbb0983147d675a032f6ca185
Piero <piero.onorati@virgilio.it>
2021-07-27 16:27:18 +0200

    add header

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,11 +7,78 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- My CSS -->
     <link rel="stylesheet" href="style.css">
+    <!-- Fontawesome CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Google font:Roboto -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap" rel="stylesheet"> 
 </head>
 <body>
 
-<img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="">
+    
+    <!-- start header -->
+    <header>
+
+        <div class="container"> 
+
+            <!-- start top-navbar -->
+                <div class="top-navbar">
+
+                    <!-- start Title -->
+                    <div class="title">
+                            <a href="#"><img src="https://www.gstatic.com/images/branding/googlelogo/svg/googlelogo_clr_74x24px.svg" alt="google logo"></a>
+                            <a href=""><h3>Privacy e termini</h3></a>
+                    </div>
+                    <!-- end Title -->
+
+                    <!-- start Google -log -->
+                    <div class="google_log">
+                        <button class="menu"><i class="fas fa-ellipsis-h"></i></button>
+                        <button class="accedi">Accedi</button>
+                    </div>
+                    <!-- end Google -log -->
+            
+            </div>
+            <!-- end top-navbar -->
+
+            <!-- start nav -->
+            <nav>
+                <ul>
+                    <li><a href="#">Introduzione</a></li>
+                    <li><a href="#">Norme sulla privacy</a></li>
+                    <li><a href="#">Termini di servizio</a></li>
+                    <li><a href="#">Tecnologie</a></li>
+                    <li><a href="#">Domande frequenti</a></li>
+                </ul>
+
+            </nav>
+            <!-- end nav -->
+
+        </div>
+
+    </header>
+    <!-- end header -->
+
+    <!-- start main -->
+    <main>
+        <div class="container"> 
+        
+        </div>
+
+    </main>
+    <!-- end main -->
+
+    <!-- start footer -->
+    <footer>
+        <div class="container"> 
+
+        </div>
+    </footer>
+    <!-- end footer -->
+
     
 </body>
 </html>
